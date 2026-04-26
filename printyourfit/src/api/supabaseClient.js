e98906ce3d907eff60fbb2b98b029ce47d981281
Skate Byrne <57212194+lumpyspaceprogrammer@users.noreply.github.com>
2026-04-26 11:12:36 -0700

    Use Vite Supabase env vars in supabase client

--- printyourfit/src/api/supabaseClient.js
+++ printyourfit/src/api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://vvhtvjxvzlxiajodktje.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2aHR2anh2emx4aWFqb2RrdGplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMjg2NzUsImV4cCI6MjA5MTcwNDY3NX0.D8-TFrQkEz8rtSfTQYvW6pOqGt1WQ8AntjM-fPkKfc4';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
